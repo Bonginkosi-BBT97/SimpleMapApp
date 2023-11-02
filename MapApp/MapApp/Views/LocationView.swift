4cacdcd5075f4aed2759f37968dcee9443b3ba18
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2023-11-02 10:44:49 +0200

    Added Map and location set up

--- MapApp/MapApp/Views/LocationView.swift
+++ MapApp/MapApp/Views/LocationView.swift
@@ -6,19 +6,20 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationView: View {
     
     @EnvironmentObject private var vm: LocationsViewModel
     
     var body: some View {
-        List {
-            ForEach(vm.locations){
-                Text($0.name)
+        ZStack{
+            Map(coordinateRegion: $vm.mapRegion)
+                .ignoresSafeArea()
             }
         }
     }
-}
+
 
 #Preview {
     LocationView()
